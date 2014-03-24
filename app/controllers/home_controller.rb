4ca6f8593618cb79b8de79e434546bc4d0b002a4
projectece595 <projectece595@gmail.com>
2014-03-24 16:31:24 -0600

    Change development DB to PG

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @au = AuthOption.new
-    @au[:name] = "license"
-    @au[:length] = 3
-    @au.valid?
-    if @au.errors.present?
-      flash[:error] = "#{@au.errors.full_messages.to_sentence}"
-    else
-      @au.save!
-      flash[:error] = "Successfully created profile. #{@au[:id]}"
-    end
+    
   end
 
   def about
