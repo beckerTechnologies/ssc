9600601f1aa51b6e44562066de0578f09cef972d
sunila <sunilasaqib1@gmail.com>
2014-03-25 17:43:11 -0600

    authOptions and lifetime options frm tbl and ary
    authoptions from table
    lifetime options from AplicationHelper class variable.
    
    if table(AuthOption) is not populated Setup/page1 and setup/create will
      throw errors.
    see docs/utility.txt to see how to load values in table from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+AuthOption.create(id:1, name:'SSN', length:9)
+AuthOption.create(id:2, name:'Area-Code', length:7)
+AuthOption.create(id:3, name:'Phone-Number', length:13)
+AuthOption.create(id:4, name:'Credit-card', length:16)
+AuthOption.create(id:5, name:'Driver-License', length:20)
+AuthOption.create(id:6, name:'Medicade', length:13)
+AuthOption.create(id:7, name:'Medicare', length:10)
