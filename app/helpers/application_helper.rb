5a1dcea5107b2bd7096311c4168e160c85560b38
sunila <sunilasaqib1@gmail.com>
2014-03-27 00:03:22 -0600

    added box position selector on setup/page3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
-  @@h = Hash[('a'..'z').to_a.zip (0..25).to_a]
+  #@@h = Hash[('a'..'z').to_a.zip (0..25).to_a]
   @@lt = [["12Hrs", 12],["1Day",24],["2Days", 48],["3Days", 72],["4Days", 96],["5Days", 120],["6Days", 144],["7Days", 168]]
   def set_ct
-    return rand(10)
+    return (rand(10)).to_s
   end
   def set_ssc (ct, ssc_val_s, ct_mask_s)
     ssc_val = ssc_val_s.split ''
-    ct_mask = ct_mask_s.split ''
+    ct_mask = ct_mask_s.split ','
     ct_mask.each do |i|
-      ind = @@h[i]
+      ind = (i).to_i
       ssc_val[ind] = ct
     end
     return ssc_val.join
--- app/models/ssc_bank.rb
+++ app/models/ssc_bank.rb
@@ -1,4 +1,5 @@
 class SscBank < ActiveRecord::Base
   belongs_to :profile
-  validates :ct_mask, presence: true, length: { minimum: 3, maximum:3}
+  VALID_MASK_REGEX = /\A[\d]+[,][\d]+[,][\d]+\z/i # 2 comas, 3 values max. 
+  validates :ct_mask, presence: true, format: { with: VALID_MASK_REGEX}
 end
