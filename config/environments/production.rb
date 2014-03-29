3becf8cbc4457d0bebb5ee165a6c5bd4f67aa9b6
projectece595 <projectece595@gmail.com>
2014-03-28 22:12:49 -0600

    Mail settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,17 @@ SSC::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.smtp_settings = {
+    #:openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,      
+    #:ssl => true,
+    :enable_starttls_auto => true,  #this is the important stuff!
+    :address        => 'smtp.gmail.com',
+    :port           => 25,
+    :domain         => 'gmail.com',
+    :authentication => 'plain',
+    :user_name      => 'notifications.ssc@gmail.com',
+    :password       => 'yahoo@123'
+  }
+
+
 end
